62a708642a9e340b1c971c0fcbc78f7274debdfd
Anuraag Yachamaneni <ayachama@purdue.edu>
2019-01-21 14:39:40 -0500

    Fix autosave

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -6,9 +6,32 @@ class Campaigns {
     this.showDataBtn = element.find("#show-data-btn");
 
     this.campaignDash.on("keyup", ".ql-editor", this.updateVariables);
+    this.campaignDash.on("keyup", ".ql-editor", this.autoSave);
     this.campaignDash.on("click", "#show-template-btn", this.showTemplate);
     this.campaignDash.on("click", "#show-data-btn", this.showData);
 
+    this.updateVariables();
+  }
+
+  autoSave() {
+    const template = window['quill-container-0'].root;
+    const templateText = template.innerText;
+    const templateHTML = template.innerHTML;
+    const campaign = window.location.href.match(/[^\/]*$/)[0];
+    
+    const data = {
+      "template": templateHTML
+    };
+
+    fetch(campaign, 
+    { 
+        method: 'PUT',  
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': Rails.csrfToken()
+        },
+    });
   }
 
   showTemplate() {
@@ -19,37 +42,6 @@ class Campaigns {
   showData() {
     $("#template").hide();
     $("#data").show();
-  }
-
-  createNewCampaign()  {
-    const name = $("#new-campaign-name").val();
-
-    if(!name || name === "") {
-      toastr.error("You need a name for your campaign!");
-      return;
-    }
-
-    const data = {
-      "name": name
-    }
-
-    fetch("campaigns/", 
-    { 
-        method: 'POST',  
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': Rails.csrfToken()
-        },
-    }).then(res => res.json())
-    .then((response) => {
-      if(response.success) {
-        location.reload();
-        toastr.success(response.message);
-      } else {
-        toastr.error(response.message);
-      }
-    });
   }
 
   updateVariables(e) {
