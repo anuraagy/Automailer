158f2044d1504a828a678fa0f5b9de7ce7cb2382
Anuraag Yachamaneni <ayachama@purdue.edu>
2019-01-26 16:54:39 -0500

    test settigns

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,17 @@ Rails.application.configure do
 
   config.assets.quiet = true
   config.active_storage.service = :local
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: “smtp.gmail.com”,
+    port: 587,
+    domain: ENV[“GMAIL_DOMAIN”],
+    authentication: “plain”,
+    enable_starttls_auto: true,
+    user_name: ENV[“GMAIL_USERNAME”],
+    password: ENV[“GMAIL_PASSWORD”]
+  }
   # config.action_view.raise_on_missing_translations = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
